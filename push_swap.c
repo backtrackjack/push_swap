42109c7c503ed9e747b107bd2120413877b17815
Jack Sellars <jsellars@F3C024.42adel.org.au>
2022-02-15 16:21:33 +1030

    Error checking done methinks

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: jsellars <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 16:12:08 by jsellars          #+#    #+#             */
-/*   Updated: 2022/02/15 09:23:19 by jsellars         ###   ########.fr       */
+/*   Updated: 2022/02/15 16:20:21 by jsellars         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,36 @@
 
 int	checkargs(int ac, char **av)
 {
-	while(ft_atoi(av[--ac]))
+	int	i;
+
+	while (--ac)
 	{
+		i = 0;
+		if ((ft_strlen(av[ac]) > 10 && av[ac][i] != '-')
+				|| (ft_strlen(av[ac]) > 11))
+		{
+			ft_putstr_fd("Error\n", 1);
+			return (1);
+		}
+		while (av[ac][i])
+		{
+			if (!ft_isdigit(av[ac][i]))
+			{
+				ft_putstr_fd("Error\n", 1);
+				return (1);
+			}
+			i++;
+		}
 		if (!ac - 1)
 			return (0);
 	}
+	ft_putstr_fd("Error\n", 1);
 	return (1);
 }
 
 int	main(int ac, char **av)
 {
-	if(ac < 2 || checkargs(ac, av))
-	{
-		ft_putstr_fd("Error\n", 1);
-	}
-	return (1);
+	if (ac < 2 || checkargs(ac, av))
+		return (1);
 	return (0);
 }
