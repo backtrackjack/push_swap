781a17efd2c3a090c71861f95767e9c422c04f7a
Jack Sellars <jsellars@F3C024.42adel.org.au>
2022-02-15 17:16:15 +1030

    error checking is so easy guys

--- push_swap.c
+++ push_swap.c
@@ -6,12 +6,33 @@
 /*   By: jsellars <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 16:12:08 by jsellars          #+#    #+#             */
-/*   Updated: 2022/02/15 16:20:21 by jsellars         ###   ########.fr       */
+/*   Updated: 2022/02/15 17:15:33 by jsellars         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "includes/push_swap.h"
+
+int	checkoverflow(char *str)
+{
+	long	result;
+	int		sign;
+	
+	sign = 1;
+	if (*str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	while (*str)
+	{
+		result = 10 * result + *str - '0';
+		str++;
+	}
+	if ((result * sign) & 0xFFFFFFFF00000000)
+		return (1);
+	return (0);
+}
 
 int	checkargs(int ac, char **av)
 {
@@ -20,20 +41,20 @@ int	checkargs(int ac, char **av)
 	while (--ac)
 	{
 		i = 0;
-		if ((ft_strlen(av[ac]) > 10 && av[ac][i] != '-')
-				|| (ft_strlen(av[ac]) > 11))
-		{
-			ft_putstr_fd("Error\n", 1);
-			return (1);
-		}
+	
 		while (av[ac][i])
 		{
-			if (!ft_isdigit(av[ac][i]))
+			if (!ft_isdigit(av[ac][i]) && av[ac][i] != '-')
 			{
 				ft_putstr_fd("Error\n", 1);
 				return (1);
 			}
 			i++;
+		}
+		if (checkoverflow(av[ac]))
+		{
+			ft_putstr_fd("Error\n", 1);
+			return (1);
 		}
 		if (!ac - 1)
 			return (0);
