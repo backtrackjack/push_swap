1f681d95c456a0772112a90b13f20535f1f2b6d0
Jack Sellars <jsellars@enee.com.au>
2022-02-16 09:26:58 +1030

    fixed overflow checking

--- push_swap.c
+++ push_swap.c
@@ -6,30 +6,33 @@
 /*   By: jsellars <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 16:12:08 by jsellars          #+#    #+#             */
-/*   Updated: 2022/02/15 17:15:33 by jsellars         ###   ########.fr       */
+/*   Updated: 2022/02/16 09:25:47 by jsellars         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "includes/push_swap.h"
+#include <limits.h>
 
 int	checkoverflow(char *str)
 {
 	long	result;
 	int		sign;
-	
+	char	*s;
+
+	s = str;
 	sign = 1;
-	if (*str == '-')
+	if (*s == '-')
 	{
 		sign = -1;
-		str++;
+		s++;
 	}
-	while (*str)
+	while (*s)
 	{
-		result = 10 * result + *str - '0';
-		str++;
+		result = 10 * result + *s - '0';
+		s++;
 	}
-	if ((result * sign) & 0xFFFFFFFF00000000)
+	if ((result * sign) < INT_MIN || (result * sign) > INT_MAX)
 		return (1);
 	return (0);
 }
@@ -41,7 +44,6 @@ int	checkargs(int ac, char **av)
 	while (--ac)
 	{
 		i = 0;
-	
 		while (av[ac][i])
 		{
 			if (!ft_isdigit(av[ac][i]) && av[ac][i] != '-')
